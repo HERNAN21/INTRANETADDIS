c1838527d999cc65276909b4ee5fc229b1a838e7
Hernan <hernanrojasutanimail.com>
2019-05-25 16:31:02 -0500

    Cambios pdf

--- controllers/reportes/boletaNotas.php
+++ controllers/reportes/boletaNotas.php
@@ -7,20 +7,6 @@ class Reporte{
 
 		$pdf->AddPage();
 		
-
-		
-        $pdf->SetFont('Arial','B',15);
-        // Movernos a la derecha
-        $pdf->Cell(80);
-        // Título
-        $pdf->Image('../../views/images/mini.png',20,15,20);
-		$pdf->Image('../../views/images/logo-Addis.png',130,15,60);
-		$pdf->Ln(11);
-		$pdf->Cell(190,10,'','B',0,'C');
-		$pdf->Ln(20);
-        $pdf->Cell(30,10,'Title',1,0,'C');
-        // Salto de línea
-        $pdf->Ln(20);
         $pdf->SetTitle("Boleta De Notas");
 		$pdf->SetLeftMargin(10);
 		$pdf->SetRightMargin(10);
@@ -28,7 +14,35 @@ class Reporte{
 		$pdf->SetTextColor(0, 0, 0);
 		$pdf->SetY(0);
 		$pdf->Cell(30);
-		
+        $pdf->SetFont('Arial','B',15);
+        // Movernos a la derecha
+        $pdf->Cell(80);
+        // Título
+        $pdf->Image('../../views/images/mini.png',20,5,20);
+		$pdf->Image('../../views/images/logo-Addis.png',130,5,60);
+		$pdf->Ln(11);
+		$pdf->Cell(190,10,'','B',0,'C');
+		$pdf->Ln(15);
+        $pdf->Cell(200,5,'BOLETA DE NOTAS',0,0,'C');
+        $pdf->SetFont('Arial','B',12);
+        $pdf->Ln(15);
+        $pdf->Cell(22,5,'CODIGO:',0,0,'L');
+        $pdf->Cell(30,5,'',1,0,'L');
+        $pdf->Ln(8);
+        $pdf->Cell(22,5,'ALUMNO:',0,0,'L');
+        $pdf->Cell(163,5,'','B',0,'L');
+        $pdf->Ln(8);
+        $pdf->Cell(55,5,'CARRERA PROFESIONAL:',0,0,'L');
+        $pdf->Cell(95,5,'','B',0,'L');
+        $pdf->Cell(20,5,'CICLO:',0,0,'C');
+        $pdf->Cell(15,5,'','B',0,'L');
+        // Salto de línea
+        $pdf->Ln(15);
+        $pdf->Cell(15,5,utf8_decode('N°'),1,0,'C');
+        $pdf->Cell(115,5,'UNIDADES DIDACTICAS',1,0,'C');
+        $pdf->Cell(25,5,'CREDITO',1,0,'C');
+        $pdf->Cell(30,5,'NOTA',1,0,'C');
+        
 		$pdf->Output();
 		
 	}
